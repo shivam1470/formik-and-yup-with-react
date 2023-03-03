39b40fb06cb24d9b25c6ed7745ea2ecc60bb7562
Shivam Mishra <shivam.mishra@wakencode.com>
2023-03-03 15:15:12 +0530

    Improved validation and form handling for nested object

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,45 @@ import { string, object } from "yup";
 import { useFormik } from "formik";
 const { TextArea } = Input;
 
+interface IAddress {
+  state: string;
+  city: string;
+  country: string;
+  zipCode: string;
+  street: string;
+}
+
+interface IInitialValue {
+  firstName: string;
+  lastName: string;
+  mobile: string;
+  email: string;
+  address: IAddress;
+  description: string;
+}
+
 // Initial value provided to formik
-const initialValue = {
+const initialValue: IInitialValue = {
   firstName: "",
   lastName: "",
   mobile: "",
   email: "",
-  address: "",
   description: "",
+  address: {
+    state: "",
+    street: "",
+    zipCode: "",
+    city: "",
+    country: "",
+  },
 };
+
+const addressSchema = object().shape({
+  state: string().required("Required"),
+  street: string().required("Required"),
+  zipCode: string().required("Required"),
+  city: string().required("Required"),
+});
 
 // Validation with the help of yup library
 const validationSchema = object().shape({
@@ -21,13 +51,13 @@ const validationSchema = object().shape({
   lastName: string().required("Required"),
   mobile: string().min(10).max(10).required("Required"),
   email: string().email().required("Required"),
-  address: string().required("Required"),
+  address: addressSchema,
   description: string(),
 });
 
 function App() {
   // Getting value, error, change handler and submit handler
-  const { values, errors, handleChange, handleSubmit } = useFormik({
+  const { values, errors, handleChange, handleSubmit, setValues } = useFormik({
     initialValues: initialValue,
     validationSchema: validationSchema,
     validateOnBlur: false,
@@ -36,6 +66,20 @@ function App() {
     },
   });
 
+  const handleAddressChange = (type: string, value: string) => {
+    setValues((prevState) => {
+      return {
+        ...prevState,
+        address: {
+          ...prevState.address,
+          [type]: value,
+        },
+      };
+    });
+  };
+
+  console.log(values, "VAL");
+  console.log(errors, "Error");
   return (
     <>
       <Card>
@@ -121,22 +165,90 @@ function App() {
               </Form.Item>
             </Col>
           </Row>
-          <Form.Item label={"Address"}>
-            <Input
-              status={errors.address && "error"}
-              value={values.address}
-              name="address"
-              onChange={handleChange}
-            />
-            <Typography
-              style={{
-                fontSize: "16px",
-                color: "#d67f6f",
-              }}
-            >
-              {errors.address}
-            </Typography>
-          </Form.Item>
+
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+              <Form.Item label={"Street"}>
+                <Input
+                  status={errors.address && "error"}
+                  value={values.address.street}
+                  onChange={(e) => {
+                    handleAddressChange("street", e.target.value);
+                  }}
+                />
+                <Typography
+                  style={{
+                    fontSize: "16px",
+                    color: "#d67f6f",
+                  }}
+                >
+                  {errors.address?.street}
+                </Typography>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label={"City"}>
+                <Input
+                  status={errors.address && "error"}
+                  value={values.address.city}
+                  onChange={(e) => {
+                    handleAddressChange("city", e.target.value);
+                  }}
+                />
+                <Typography
+                  style={{
+                    fontSize: "16px",
+                    color: "#d67f6f",
+                  }}
+                >
+                  {errors.address?.city}
+                </Typography>
+              </Form.Item>
+            </Col>
+          </Row>
+
+          <Row gutter={[16, 16]}>
+            <Col span={12}>
+              <Form.Item label={"State"}>
+                <Input
+                  status={errors.address && "error"}
+                  value={values.address.state}
+                  onChange={(e) => {
+                    handleAddressChange("state", e.target.value);
+                  }}
+                />
+                <Typography
+                  style={{
+                    fontSize: "16px",
+                    color: "#d67f6f",
+                  }}
+                >
+                  {errors.address?.state}
+                </Typography>
+              </Form.Item>
+            </Col>
+            <Col span={12}>
+              <Form.Item label={"Zip Code"}>
+                <Input
+                  status={errors.address && "error"}
+                  value={values.address.zipCode}
+                  name="address"
+                  onChange={(e) => {
+                    handleAddressChange("zipCode", e.target.value);
+                  }}
+                />
+                <Typography
+                  style={{
+                    fontSize: "16px",
+                    color: "#d67f6f",
+                  }}
+                >
+                  {errors.address?.zipCode}
+                </Typography>
+              </Form.Item>
+            </Col>
+          </Row>
+
           <Form.Item label={"Description"}>
             <TextArea
               value={values.description}
